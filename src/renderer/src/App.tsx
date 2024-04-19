2e2e87ba006272cd5eb977aeadcd7581e4a47f0d
luminous <910667665@qq.com>
2024-04-19 17:15:44 +0800

    component:预览note组件实现

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,11 @@
-import { ActionButtonContainer, Content, DraggableTopBar, RootLayout, Sidebar } from './components'
+import {
+  ActionButtonContainer,
+  Content,
+  DraggableTopBar,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from './components'
 
 const App = () => {
   return (
@@ -6,9 +13,10 @@ const App = () => {
       <DraggableTopBar></DraggableTopBar>
       <RootLayout>
         <Sidebar className="p-2">
-          <ActionButtonContainer className="flex justify-between items-center mt-1"></ActionButtonContainer>
+          <ActionButtonContainer className="flex justify-between items-center mt-1" />
+          <NotePreviewList className="mt-3 space-y-2" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">暂无内容</Content>
       </RootLayout>
     </>
   )
--- src/renderer/src/store/mocks/index.ts
+++ src/renderer/src/store/mocks/index.ts
@@ -2,7 +2,7 @@ import { NoteInfo } from '@/shared/models'
 
 export const notesMock: NoteInfo[] = [
   {
-    title: 'Note 1',
+    title: 'welcome',
     lastEditTime: new Date().getTime()
   },
   {
