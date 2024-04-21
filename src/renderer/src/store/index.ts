d7d547632b255fe21941c9200ba6b1e935d8abba
luminous <910667665@qq.com>
2024-04-21 14:16:55 +0800

    feat: store实现笔记的选择/新增/删除功能

---
+++ src/renderer/src/store/index.ts
@@ -0,0 +1,52 @@
+import { notesMock } from '@/store/mocks'
+import { NoteInfo } from '@renderer/shared/models'
+import { atom } from 'jotai'
+
+export const notesAtom = atom<NoteInfo[]>(notesMock)
+
+export const selectedNoteIndexAtom = atom<number | null>(null)
+
+export const selectedNoteAtom = atom<NoteInfo | null>((get) => {
+  const notes = get(notesAtom)
+  const selectedNoteIndex = get(selectedNoteIndexAtom)
+
+  if (selectedNoteIndex == null) {
+    return null
+  }
+
+  const selectedNote = notes[selectedNoteIndex]
+
+  return {
+    ...selectedNote,
+    content: `hello from note${selectedNoteIndex}!`
+  }
+})
+
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+
+  const title = `New Note ${notes.length + 1}`
+
+  const newNote: NoteInfo = {
+    title,
+    lastEditTime: Date.now()
+  }
+
+  set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+
+  set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) return null
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.title !== selectedNote.title)
+  )
+
+  set(selectedNoteIndexAtom, null)
+})
